e90c0759ba0dc3c11a061179303b3535acf9a6a7
Esteffs <42493575+Esteffs@users.noreply.github.com>
2018-12-01 21:56:48 -0200

    POO
    POO Exercises

---
+++ #6/Calculator.java
@@ -0,0 +1,16 @@
+//Methods
+public class Calculator 
+{
+    public static final double PI = 3.14159; //final = variable doesnt change
+
+    public static double circumference(double radius)
+    {
+        return 2.0 * PI * radius;
+    }
+
+    public static double volume(double radius)
+    {
+        return 4.0 * PI * radius * radius* radius / 3.0;
+    }
+    
+}
